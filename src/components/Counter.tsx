90d75a31bbbceede1e155db9a779fafcf7f4c247
ha08ya.a <ha08ya@gmail.com>
2021-10-28 02:42:08 +0900

    [Mod] prevCount
    同じ更新タイミングでも同じstateを変更

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,8 +2,16 @@ import React, { useState } from 'react';
 
 const Counter: React.VFC = () => {
   const [count, setCount] = useState(0);
-
-  return <button onClick={() => setCount(count + 1)}>{count}</button>;
+  return (
+    <button
+      onClick={() => {
+        setCount((prevCount) => prevCount + 1); // 最新のcountを取得し変更(?)
+        setCount((prevCount) => prevCount + 1);
+      }}
+    >
+      {count}
+    </button>
+  );
 };
 
 export default Counter;
