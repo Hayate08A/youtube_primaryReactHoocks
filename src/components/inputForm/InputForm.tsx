264ef05af8a298b32f89890b26d66c902486b007
ha08ya.a <ha08ya@gmail.com>
2021-10-28 03:11:22 +0900

    [Add] name管理

--- src/components/inputForm/InputForm.tsx
+++ src/components/inputForm/InputForm.tsx
@@ -9,8 +9,28 @@ export const InputForm: React.VFC = () => {
   const [name, setName] = useState<TName>({ first: '', last: '' });
   return (
     <div>
-      <input />
-      <input />
+      <input
+        value={name.first}
+        onChange={(e) =>
+          setName((prevName) => ({
+            first: e.target.value,
+            last: prevName.last,
+          }))
+        }
+      />
+      <input
+        value={name.last}
+        onChange={(e) =>
+          setName((prevName) => ({
+            first: prevName.first,
+            last: e.target.value,
+          }))
+        }
+      />
+      <p>
+        {name.first}
+        {name.last}
+      </p>
     </div>
   );
 };
