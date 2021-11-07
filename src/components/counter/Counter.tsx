8b42a60f3514cd07c5d8bfeef251ca486ebd6ebe
ha08ya.a <ha08ya@gmail.com>
2021-11-07 17:18:55 +0900

    [WIP] 第3回-1：useEffectの理解前編 _ buttom2追加

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,22 +1,30 @@
 import React, { useEffect, useState } from 'react';
 
 const Counter: React.VFC = () => {
-  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-useEffect(() => {
-  console.log("effect");
-  
-})
+  useEffect(() => {
+    console.log('effect');
+  }, []);
 
   return (
-    <button
-      onClick={() => {
-        setCount((prevCount) => prevCount + 1); // 最新のcountを取得し変更(?)
-        setCount((prevCount) => prevCount + 1);
-      }}
-    >
-      {count}
-    </button>
+    <div>
+      <button
+        onClick={() => {
+          setCount1((prevCount) => prevCount + 1);
+        }}
+      >
+        {count1}
+      </button>
+      <button
+        onClick={() => {
+          setCount2((prevCount) => prevCount + 1);
+        }}
+      >
+        {count2}
+      </button>
+    </div>
   );
 };
 
