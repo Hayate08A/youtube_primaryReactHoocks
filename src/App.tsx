c7de6aa3b154fc17f94373b0d3c4e72432f1d22d
ha08ya.a <ha08ya@gmail.com>
2021-11-20 23:22:43 +0900

    Auto stash before merge of "main" and "origin/main"

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Counter from './components/counter/Counter';
 // import InputForm from './components/inputForm/InputForm';
 
 function App() {
+  const [isDisplay, setIsDisplay] = useState(true);
+  const handleDisplay = () => {
+    setIsDisplay(!isDisplay);
+  };
   return (
     <div className='App'>
       <h2>#3 useEffect</h2>
-      <Counter />
+      <button onClick={() => handleDisplay()}>
+        {isDisplay ? 'コンポーネントを非表示' : 'コンポーネントを表示'}
+      </button>
+      {isDisplay && <Counter />}
       {/* <InputForm /> */}
     </div>
   );
