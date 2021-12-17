fce653ef984ff3bb09e1476e40b88532a092b3a4
ha08ya <ha08ya@gmail.com>
2021-12-17 15:42:32 +0900

    [WIP] 第3回-2：useEffectの理解後編 / 6:20 { type追加

--- src/components/itemList/ItemList.tsx
+++ src/components/itemList/ItemList.tsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+type TTodo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+type TPostType = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+type TItem = {
+  id: number;
+  title: string;
+};
 
 const ItemList: React.VFC = () => {
+  const [items, setItems] = useState<TItem[]>([]);
   return <div></div>;
 };
 
