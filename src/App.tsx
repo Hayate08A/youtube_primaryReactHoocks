242126dc0348a963a950216de92f32d22de49dfe
ha08ya <ha08ya@gmail.com>
2021-12-27 20:47:14 +0900

    [WIP] 第4回：useContextの理解 { App.tsxにAuthContext追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React, { useState } from "react";
-// import logo from './logo.svg';
+import React from "react";
 import "./App.css";
-import Counter from "./components/counter/Counter";
-import ItemList from "./components/itemList/ItemList";
-// import InputForm from './components/inputForm/InputForm';
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
-  const [isDisplay, setIsDisplay] = useState(true);
-  const handleDisplay = () => {
-    setIsDisplay(!isDisplay);
-  };
   return (
-    <div className="App">
-      <h2>#4 useContext</h2>
-      {/* <button onClick={() => handleDisplay()}>
-        {isDisplay ? "コンポーネントを非表示" : "コンポーネントを表示"}
-      </button> */}
-      {/* {isDisplay && <Counter />} */}
-      {/* <InputForm /> */}
-      <ItemList />
-    </div>
+    <AuthProvider
+      childlen={
+        <div className="App">
+          <h2>#4 useContext</h2>
+        </div>
+      }
+    />
   );
 }
 
