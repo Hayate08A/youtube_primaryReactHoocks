e1d37a703bc57003a4a002774917f38873ea7cd1
ha08ya <ha08ya@gmail.com>
2021-12-27 21:23:32 +0900

    [Add] 第4回：useContextの理解 { AuthDisplay修正

--- src/components/userAuth/AuthDisplay.tsx
+++ src/components/userAuth/AuthDisplay.tsx
@@ -3,7 +3,7 @@ import { AuthContext } from "../../context/AuthContext";
 
 const AuthDisplay: React.VFC = () => {
   const auth = useContext(AuthContext);
-  return <div>{auth ? <p>ログイン中</p> : <p>ログアウト中</p>}</div>;
+  return <div>{auth?.userAuth ? <p>ログイン中</p> : <p>ログアウト中</p>}</div>;
 };
 
 export default AuthDisplay;
